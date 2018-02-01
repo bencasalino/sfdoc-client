c069ebcc81db2a47427dd27885a16ea46eec95e0
Ben Casalino <bencasalino@gmail.com>
2018-01-31 19:47:41 -0700

    styling

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return <div className="AboutSection">
+      <p>
+        This is a directory containting all indoor/outdoor soccer facilities in Colorado details and information. <img className="Ball" src="/assets/logo.png" alt="Logo" />
+        <hr />
+        {/* <img className="Ball" src="/assets/logo.png" alt="Logo" /> */}
+      </p>
+    </div>;
+}
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return <header>
+      <a href="github.com/bencasalino">
+        <img className="Github animated slideInDown" src="/assets/github.png" alt="Github Logo" />
+      </a>
+      <img className="LogoImage animated slideInLeft" src="/assets/field.png" alt="Logo" />
+      <span className="Logo">
+        Soccer Field Directory<br /> of Colorado
+      </span>
+      {/* <img className="Ball" src="/assets/logo.png" alt="Logo" /> */}
+      {/* Soccer Field Directory of Colorado */}
+    </header>;
+}
